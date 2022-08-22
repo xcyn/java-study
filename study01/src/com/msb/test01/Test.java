a41dde1a7879a1ec979ba8c0a4e5dae2ec163d64
apple <xiongchuanyu@qiyi.com>
2022-08-22 16:17:22 +0800

    feat: first commit

---
+++ study01/src/com/msb/test01/Test.java
@@ -0,0 +1,9 @@
+package com.msb.test01;
+
+public class Test {
+    //  main方法: 程序入口
+    public static void main(String[] args) {
+        System.out.println("hello-word,这是我的第一段java程序");
+        System.out.println("🥱");
+    }
+}
---
+++ study01/src/com/msb/test02/TestVar.java
@@ -0,0 +1,9 @@
+package com.msb.test02;
+
+public class TestVar {
+    public static void main(String[] args) {
+        int age;
+        age = 1100;
+        System.out.println(age);
+    }
+}
